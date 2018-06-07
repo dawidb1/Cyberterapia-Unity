08a9819ab863589b6185a56dd4702386b9137294
Dawid Barański <dawid.baranski.96@gmail.com>
2018-06-07 18:11:44 +0200

    snake game added

---
+++ Assets/SnakeGame/MyTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyTimer : MonoBehaviour {
+	public static float czas = 0;
+	public Text czas_txt;
+	public GameObject plutno;
+	// Use this for initialization
+
+	void Awake () {
+		//DontDestroyOnLoad (plutno);
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		czas_txt= gameObject.GetComponent<Text>();
+		czas += Time.deltaTime;
+		czas_txt.text=string.Format("czas: {0}",czas);
+	}
+}
